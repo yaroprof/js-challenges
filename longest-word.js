bdfc0cc19617570d3cf17a0c086dfef30fbd3ca3
Yaroslav <crosskyiv@gmail.com>
2022-06-11 17:20:21 +0300

    longest word

--- longest-word.js
+++ longest-word.js
@@ -15,15 +15,32 @@
 //console.log( longestWord('I doing fitness exercises every morning'));
 //console.log( longestWord('I went straight to the bu station'));
 
+// 02 version: sort() method
 
+//function longestWord(str){
+//	let words = str.split(' ')
+//	let longestWord = words.sort((a, b) =>{
+//		return b.length - a.length
+//	})
 
+//	return longestWord[0]
+//}
 
+//console.log( longestWord('I doing fitness exercises every morning'));
+//console.log( longestWord('I went straight to the bus station'));
 
+// 03 version: reduce() method
 
+//function longestWord(str){
+//	let words = str.split(' ')
+//	let longWords = words.reduce( function(longest, current){
+//		return current.length > longest.length ? current: longest
+//	}, '')
+//	return longWords
+//}
 
-
-
-
+//console.log( longestWord('I doing fitness exercises every morning'));
+//console.log( longestWord('I went straight to the bus station'));
 
 
 

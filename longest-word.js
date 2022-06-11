821371f8e64ba3106fb07f080d1fef7f3299271f
Yaroslav <crosskyiv@gmail.com>
2022-06-11 08:25:08 +0300

    01 version

---
+++ longest-word.js
@@ -0,0 +1,31 @@
+// 01 version: split an array, for loop an dcomparisson with longest word in a string
+//function longestWord(str){
+//	let words = str.split(' ')
+//	let longestWord = ''
+
+//	for(let word of words){
+//		if( word.length > words.length ){
+//			longestWord = word
+//		}
+//	}
+
+//	return longestWord
+//}
+
+//console.log( longestWord('I doing fitness exercises every morning'));
+//console.log( longestWord('I went straight to the bu station'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
